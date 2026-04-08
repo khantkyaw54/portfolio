884407320b30fd3c2014ed157ad887cb7f4e59f9
khantkyaw54 <khantkyaw7291@gmail.com>
2026-04-08 09:42:59 +0900

    portfolio

---
+++ script.js
@@ -0,0 +1,89 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".header__nav-list");
+
+if (hamburger && nav) {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+}
+
+
+
+const text = ["Frontend Developer", "React Learner", "Future Engineer in Japan"];
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+    currentText = text[i];
+
+    if (isDeleting) {
+        j--;
+    } else {
+        j++;
+    }
+
+    document.getElementById("typing-text").textContent = currentText.substring(0, j);
+
+    if (!isDeleting && j === currentText.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % text.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
+
+// back to top button
+const toTop = document.getElementById("toTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
+
+toTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+const items = document.querySelectorAll(".stagger");
+
+window.addEventListener("scroll", () => {
+    items.forEach((el, index) => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 50) {
+            setTimeout(() => {
+                el.classList.add("show");
+            }, index * 150); // delay
+        }
+    });
+});
+
+const zooms = document.querySelectorAll(".zoom");
+
+window.addEventListener("scroll", () => {
+    zooms.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+});
